e86a1692f17337ce6379f94d8461f8336d25d284
Joana Parente <jrparente@gmail.com>
2024-03-18 11:22:19 +0000

    Separated the Instagram feed into its own component. Fixed grid style.

---
+++ app/_components/InstaFeed.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface InstagramPost {
+  id: string;
+  caption: string;
+  media_url: string;
+  media_type: string;
+  timestamp: string;
+  permalink: string;
+}
+
+export default async function InstaFeed() {
+  let instagramFeed = null;
+  let error = null;
+
+  try {
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp,permalink&access_token=${process.env.IG_TOKEN}`;
+    const data = await fetch(url);
+    console.log("data", data);
+    if (!data.ok) {
+      throw new Error("Failed to fetch Instagram feed");
+    }
+
+    instagramFeed = await data.json();
+    console.log("Instagram feed:", instagramFeed);
+  } catch (err: any) {
+    console.error("Error fetching Instagram feed:", err.message);
+    error = err.message;
+  }
+  return (
+    <>
+      {error && <p className="text-red-500">{error}</p>}
+
+      {instagramFeed && (
+        <section className="w-full flex flex-col justify-center items-center">
+          <h2 className="text-2xl font-semibold">Instagram Feed:</h2>
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {instagramFeed.data.map((post: InstagramPost) => (
+              <div key={post.id} className="relative group w-full h-[300px]">
+                <Link
+                  href={post.permalink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="relative"
+                >
+                  {post.media_type === "VIDEO" ? (
+                    <video
+                      src={post.media_url}
+                      controls={false}
+                      className="w-full h-full object-cover"
+                    />
+                  ) : (
+                    <Image
+                      src={post.media_url}
+                      alt={post.caption}
+                      className="w-full h-full object-cover"
+                      width={300}
+                      height={300}
+                      priority
+                    />
+                  )}
+
+                  <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-300 bg-black bg-opacity-50 flex items-center justify-center p-4 w-full h-[300px]">
+                    <p className="text-white text-center text-xs truncate">
+                      {post.caption}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
-interface InstagramPost {
-  id: string;
-  caption: string;
-  media_url: string;
-  media_type: string;
-  timestamp: string;
-  permalink: string;
-}
+import InstaFeed from "./_components/InstaFeed";
 
 export default async function Home() {
-  let instagramFeed = null;
-  let error = null;
-
-  try {
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`;
-    const data = await fetch(url);
-    console.log("data", data);
-    if (!data.ok) {
-      throw new Error("Failed to fetch Instagram feed");
-    }
-
-    instagramFeed = await data.json();
-    console.log("Instagram feed:", instagramFeed);
-  } catch (err: any) {
-    console.error("Error fetching Instagram feed:", err.message);
-    error = err.message;
-  }
-
   return (
-    <main className="flex min-h-screen max-w-5xl mx-auto flex-col items-center gap-8 p-24">
+    <main className="flex min-h-screen max-w-7xl mx-auto flex-col items-center gap-8 p-24">
       <h1 className="text-4xl font-bold text-center">
         How to Add Instagram Photos to Your NextJS Site
       </h1>
@@ -39,45 +11,16 @@ export default async function Home() {
         Instagram Basic Display API. You can read the full tutorial on{" "}
         <a
           href="https://www.medium.com"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-blue-500 underline hover:no-underline hover:text-blue-700"
         >
           Medium
         </a>
         .
       </p>
-      {error && <p className="text-red-500">{error}</p>}
-      {instagramFeed && (
-        <section className="flex flex-col justify-center items-center">
-          <h2 className="text-2xl font-semibold">Instagram Feed:</h2>
-          <div className="grid grid-cols-3 gap-4">
-            {instagramFeed.data.map(
-              (post: InstagramPost) =>
-                post.media_type === "IMAGE" && (
-                  <div key={post.id} className="relative group">
-                    <Link
-                      href={post.permalink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="relative"
-                    >
-                      <Image
-                        src={post.media_url}
-                        alt={post.caption}
-                        className="w-full h-auto"
-                        width={300}
-                        height={300}
-                      />
 
-                      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-300 bg-black bg-opacity-50 flex items-center justify-center">
-                        <p className="text-white text-center">{post.caption}</p>
-                      </div>
-                    </Link>
-                  </div>
-                )
-            )}
-          </div>
-        </section>
-      )}
+      <InstaFeed />
     </main>
   );
 }
